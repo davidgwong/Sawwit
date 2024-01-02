24d671c6ef826f208e096c206237607537e58e72
David Wong <11532851+davidgwong@users.noreply.github.com>
2024-01-01 21:33:07 -0800

    Add loader feedback for register and sign in page

--- frontend/src/views/Login.page.tsx
+++ frontend/src/views/Login.page.tsx
@@ -1,14 +1,15 @@
 import {
   TextInput,
   PasswordInput,
-  Checkbox,
   Anchor,
   Paper,
   Title,
   Text,
   Container,
-  Group,
   Button,
+  LoadingOverlay,
+  Box,
+  Alert,
 } from "@mantine/core";
 import classes from "./Login.page.module.css";
 import DOMAIN from "../services/endpoint";
@@ -16,13 +17,19 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "@mantine/form";
 import { useUser } from "../store/store";
+import { useState } from "react";
 
 const LoginPage = () => {
   const { setUsername, setUserId, setAuthenticated } = useUser();
+  const [loading, setLoading] = useState(false);
+  const [loginAttempt, setLoginAttempt] = useState({
+    attempted: false,
+    message: "",
+  });
 
   const form = useForm({
     initialValues: {
-      uname: "",
+      username: "",
       password: "",
     },
   });
@@ -32,18 +39,21 @@ const LoginPage = () => {
   const handleLogin = async (e: any) => {
     e.preventDefault();
     try {
+      setLoading(true);
+
       const res = await axios.post(`${DOMAIN}/auth/login`, form.values);
-      console.log(
-        "handle login res username: " +
-          res.data.username +
-          " res authenticated: " +
-          res.data.isAuthenticated
-      );
       setUsername(res.data.username);
       setUserId(res.data.userId);
       setAuthenticated(res.data.isAuthenticated);
       navigate("/");
-    } catch (err) {}
+    } catch (err) {
+      setLoginAttempt({
+        attempted: true,
+        message: "Username or password is invalid. Please try again.",
+      });
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -62,31 +72,38 @@ const LoginPage = () => {
         </Anchor>
       </Text>
 
+      {loginAttempt.attempted ? (
+        <Alert variant="light" color="red" mt={30}>
+          {loginAttempt.message}
+        </Alert>
+      ) : (
+        <></>
+      )}
+
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <form onSubmit={handleLogin}>
-          <TextInput
-            label="Username"
-            placeholder="Your username"
-            required
-            {...form.getInputProps("uname")}
-          />
-          <PasswordInput
-            label="Password"
-            placeholder="Your password"
-            required
-            mt="md"
-            {...form.getInputProps("password")}
-          />
-          <Group justify="space-between" mt="lg">
-            <Checkbox label="Remember me" />
-            <Anchor component="button" size="sm">
-              Forgot password?
-            </Anchor>
-          </Group>
-          <Button type="submit" fullWidth mt="xl">
-            Sign in
-          </Button>
-        </form>
+        <Box pos="relative">
+          <LoadingOverlay visible={loading} zIndex={1000} />
+          <form onSubmit={handleLogin}>
+            <TextInput
+              label="Username"
+              placeholder="Your username"
+              disabled={loading}
+              required
+              {...form.getInputProps("username")}
+            />
+            <PasswordInput
+              label="Password"
+              placeholder="Your password"
+              disabled={loading}
+              required
+              mt="md"
+              {...form.getInputProps("password")}
+            />
+            <Button type="submit" fullWidth mt="xl" disabled={loading}>
+              Sign in
+            </Button>
+          </form>
+        </Box>
       </Paper>
     </Container>
   );
--- frontend/src/views/Register.page.tsx
+++ frontend/src/views/Register.page.tsx
@@ -8,6 +8,8 @@ import {
   Title,
   Text,
   Alert,
+  Box,
+  LoadingOverlay,
 } from "@mantine/core";
 import classes from "./Register.page.module.css";
 import { useNavigate } from "react-router-dom";
@@ -32,9 +34,12 @@ const RegisterPage = () => {
     message: "",
   });
 
+  const [loading, setLoading] = useState(false);
+
   const handleRegister = async (e: any) => {
     e.preventDefault();
     try {
+      setLoading(true);
       const res = await axios.post(`${DOMAIN}/users/`, form.values);
       setRegisterAttempt({
         attempted: true,
@@ -49,6 +54,8 @@ const RegisterPage = () => {
         success: false,
         message: err.response.data,
       });
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -89,24 +96,29 @@ const RegisterPage = () => {
       )}
 
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <form onSubmit={handleRegister}>
-          <TextInput
-            label="Username"
-            placeholder="Your username"
-            required
-            {...form.getInputProps("username")}
-          />
-          <PasswordInput
-            label="Password"
-            placeholder="Your password"
-            required
-            mt="md"
-            {...form.getInputProps("password")}
-          />
-          <Button type="submit" fullWidth mt="xl">
-            Sign in
-          </Button>
-        </form>
+        <Box pos="relative">
+          <LoadingOverlay visible={loading} zIndex={1000} />
+          <form onSubmit={handleRegister}>
+            <TextInput
+              label="Username"
+              placeholder="Your username"
+              disabled={loading}
+              required
+              {...form.getInputProps("username")}
+            />
+            <PasswordInput
+              label="Password"
+              placeholder="Your password"
+              disabled={loading}
+              required
+              mt="md"
+              {...form.getInputProps("password")}
+            />
+            <Button type="submit" fullWidth mt="xl" disabled={loading}>
+              Register
+            </Button>
+          </form>
+        </Box>
       </Paper>
     </Container>
   );
